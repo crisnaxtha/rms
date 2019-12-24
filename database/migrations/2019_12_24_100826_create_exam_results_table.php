a02fa14243dbcf11f549a764ba542243f6e0f02f
LOCHAN <krishnashrestha49@gmail.com>
2019-12-24 15:59:17 +0545

    working on exam result model and db done

---
+++ database/migrations/2019_12_24_100826_create_exam_results_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('attendance');
+            $table->unsignedInteger('exam_schedules_id')->nullable();
+            $table->unsignedInteger('student_id')->nullable();
+            $table->float('get_marks', 10, 2)->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exam_results');
+    }
+}
