f3acc85020273f3224dcd01360c74b284b8f6543
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2020-01-23 13:35:30 +0545

    added Database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(GradesTableSeeder::class);
+        $this->call(SubjectsTableSeeder::class);
+        $this->call(MyClassesTableSeeder::class);
     }
 }
