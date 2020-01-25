f080706c3e4c58fd40ea9c90165caed8c6aba4e8
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2020-01-25 16:12:54 +0545

    shool form removed

--- resources/views/dsms/school/includes/form-create.blade.php
+++ resources/views/dsms/school/includes/form-create.blade.php
@@ -2,8 +2,8 @@
 dm_postform(URL::route($_base_route.'.store'), 'POST');
 // dm_input('text','code', "School Unique Code(<em style='color:red'>*</em>)", 'code');
 dm_input('text','title', "Name(<em style='color:red'>*</em>)", 'title');
-dm_input('text','established', "Established Date", 'established');
-dm_textarea('description', "Description", 'description');
+// dm_input('text','established', "Established Date", 'established');
+// dm_textarea('description', "Description", 'description');
 dm_hsubmit('Submit', URL::route($_base_route.'.index'), 'Cancel');
 dm_closeform();
 ?>
