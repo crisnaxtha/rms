fd49a3ed85f1c2c0647f75cf72d467a6f87bfb9b
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2020-03-14 12:41:35 +0545

    create Report MVC

---
+++ database/migrations/2020_03_14_063806_create_reports_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('session_id')->nullable();
+            $table->unsignedInteger('school_class_section_id')->nullable();
+            $table->unsignedInteger('exam_id')->nullable();
+            $table->unsignedInteger('student_id')->nullable();
+
+            $table->float('total_th_marks', 10, 2)->nullable();
+            $table->float('total_pr_marks', 10, 2)->nullable();
+            $table->float('grand_total_marks', 10,2)->nullable();
+            $table->float('percentage', 10,1)->nullable();
+            $table->float('gpa', 10,1)->nullable();
+            $table->string('results')->nullable();
+            $table->text('description')->nullable();
+
+            $table->foreign('session_id')->references('id')->on('sessions')
+                    ->onDelete('cascade');
+            $table->foreign('school_class_section_id')->references('id')->on('school_class_sections')
+                    ->onDelete('cascade');
+            $table->foreign('exam_id')->references('id')->on('exams')
+                    ->onDelete('cascade');
+            $table->foreign('student_id')->references('id')->on('students')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
