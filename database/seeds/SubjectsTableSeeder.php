c38496456e117f8e216ed2134a2be386dc6fc201
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2020-03-17 22:19:53 +0545

    worked on Subject Seeder

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -15,56 +15,74 @@ class SubjectsTableSeeder extends Seeder
         Subject::create([
             'title' =>'Compulsory English',
             'theory_full_marks'=>'75',
+            'theory_pass_marks'=>'35',
             'practical_full_marks'=>'25',
+            'practical_pass_marks'=>'10',
             'credit_hour'=>'4',
         ]);
 
         Subject::create([
             'title' =>'Compulsory Nepali',
             'theory_full_marks'=>'75',
+            'theory_pass_marks'=>'35',
             'practical_full_marks'=>'25',
+            'practical_pass_marks'=>'10',
             'credit_hour'=>'4',
         ]);
         Subject::create([
             'title' =>'Compulsory Mathematics',
             'theory_full_marks'=>'100',
+            'theory_pass_marks'=>'40',
             'practical_full_marks'=>'',
+            'practical_pass_marks'=>'',
             'credit_hour'=>'4',
         ]);
         Subject::create([
             'title' =>'Compulsory Social Studies and Population Education',
             'theory_full_marks'=>'75',
+            'theory_pass_marks'=>'35',
             'practical_full_marks'=>'25',
+            'practical_pass_marks'=>'10',
             'credit_hour'=>'4',
         ]);
         Subject::create([
             'title' =>'Compulsory Science and Environment Education',
             'theory_full_marks'=>'75',
+            'theory_pass_marks'=>'35',
             'practical_full_marks'=>'25',
+            'practical_pass_marks'=>'10',
             'credit_hour'=>'4',
         ]);
         Subject::create([
             'title' =>'Compulsory Moral Education',
             'theory_full_marks'=>'50',
+            'theory_pass_marks'=>'20',
             'practical_full_marks'=>'20',
+            'practical_pass_marks'=>'10',
             'credit_hour'=>'2',
         ]);
         Subject::create([
             'title' =>'Compulsory Health and Physical Education',
             'theory_full_marks'=>'50',
+            'theory_pass_marks'=>'20',
             'practical_full_marks'=>'20',
+            'practical_pass_marks'=>'10',
             'credit_hour'=>'2',
         ]);
         Subject::create([
             'title' =>'Compulsory Occupation, Business and Technology Education',
             'theory_full_marks'=>'75',
+            'theory_pass_marks'=>'35',
             'practical_full_marks'=>'25',
+            'practical_pass_marks'=>'10',
             'credit_hour'=>'4',
         ]);
         Subject::create([
             'title' =>'Optional Computer Education',
             'theory_full_marks'=>'75',
+            'theory_pass_marks'=>'35',
             'practical_full_marks'=>'25',
+            'practical_pass_marks'=>'10',
             'credit_hour'=>'4',
         ]);
 
