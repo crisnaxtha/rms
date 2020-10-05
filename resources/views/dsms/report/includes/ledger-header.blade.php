cf3922ab81e7fdc5a65dc051971689d4d43ede99
Krishna Shrestha <krishnashrestha49@gmail.com>
2020-10-06 00:13:36 +0545

    Small change on ledger header

--- resources/views/dsms/report/includes/ledger-header.blade.php
+++ resources/views/dsms/report/includes/ledger-header.blade.php
@@ -3,6 +3,7 @@
     <h3><b>{{ $data['ms_setting']->title_1 }}</b></h3>
     <h6>{{ $data['ms_setting']->title_2 }}</h6>
     <h6>@if(isset( $data['school_id'])) {{ dm_getSchool($data['school_id'])->title }} @endif</h6>
+    <h6>Class:&nbsp;@if(isset( $data['class_id'])) {{ dm_getClass($data['class_id'])->title }} @endif</h6>
     <h6>@if(isset($data['exam_id'])){{ dm_getExam($data['exam_id'])->title}} @endif, @if(isset($data['session_id'])){{ dm_getSession($data['session_id'])->session }}@endif</h6>
     <h4 style="text-decoration: underline;;"><b>{{ $_panel }}</b></h4>
     @else
