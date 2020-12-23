dd9cb5a906a1fea71f4e6db93652a9bcee71c126
Krishna Shrestha <krishnashrestha49@gmail.com>
2020-12-23 22:49:59 +0545

    Added Footer note on footer

--- resources/views/dsms/includes/footer.blade.php
+++ resources/views/dsms/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="site-footer">
         <div class="text-center">
-            {{ date('Y') }} &copy; RMS. Page rendered in {{ date('s', $_SERVER['REQUEST_TIME_FLOAT']) }} Seconds.@if(isset(Auth::user()->last_login_at)) Your IP Address is {{ Auth::user()->last_login_ip }} & Login Time is {{ Auth::user()->last_login_at }}<strong>[{{ Auth::user()->last_login_at->diffForHumans() }}]</strong>@endif
+            {{ date('Y') }} &copy; RMS v2.0. Page rendered in {{ date('s', $_SERVER['REQUEST_TIME_FLOAT']) }} Seconds.
             <a href="#" class="go-top">
                 <i class="fa fa-angle-up"></i>
             </a>
